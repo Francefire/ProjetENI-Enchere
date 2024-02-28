c793450f0b3a55df69ef564e2a3b4bbf9ef09556
Francefire <francefire.contact@gmail.com>
2024-02-28 12:34:27 +0100

    ajout servlet

---
+++ src/main/java/fr/eni/projetencheres/servlets/ServletUser.java
@@ -0,0 +1,34 @@
+package fr.eni.projetencheres.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletUser
+ */
+@WebServlet("/users")
+public class ServletUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String paramId = request.getParameter("id");
+		System.out.println(paramId);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
