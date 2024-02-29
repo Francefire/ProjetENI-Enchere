490dcab59741b0efd8cb31b6836f4ef7b7eb49ab
DEVMJ00 <dev.mj@hotmail.com>
2024-02-29 15:39:29 +0100

    Modifications sur ma branche # WARNING: head commit changed in the meantime
    Merge remote-tracking branch 'origin' into roots

--- src/main/java/fr/eni/projetencheres/bll/UserManager.java
+++ src/main/java/fr/eni/projetencheres/bll/UserManager.java
@@ -5,4 +5,22 @@ import fr.eni.projetencheres.dal.DAOFactory;
 import fr.eni.projetencheres.dal.UserDAO;
 
 public class UserManager {
+	//on souhaite créer une unique instance du UserManager, de sorte à ce que ce soit toujours le même qui soit utilisé
+	private static UserManager instance = null;
+	
+	public static UserManager getInstance() {
+		if(instance==null) {	//On s'assure qu'il n'y a pas déjà d'instance déjà crée
+			instance = new UserManager(); //dans ce cas, on créé une instance de l'UserManager. 
+		}
+		return instance ; 
+	}
+		
+	//création d'une méthode qui se sert de la DAO factory pour créer un nouvel user. 
+		public void createUser (User u) {
+			DAOFactory.getUserDAO().insert(u);
+		}
+		
+		
+	// création d'une méthode pour
+		
 }
