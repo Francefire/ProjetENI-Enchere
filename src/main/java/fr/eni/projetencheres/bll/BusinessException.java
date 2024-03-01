1bbab86a85415dcdbe3e9769d74bcee0e23f892b
DEVMJ00 <dev.mj@hotmail.com>
2024-03-01 11:21:37 +0100

    Ajout fichier business exception

--- src/main/java/fr/eni/projetencheres/bll/BusinessException.java
+++ src/main/java/fr/eni/projetencheres/bll/BusinessException.java
@@ -6,10 +6,13 @@ public class BusinessException extends Exception {
 	public static final String DAL_INSERT_ARTICLE_SQLEXCEPTION = "Erreur lors de l'insertion de l'article dans la base de donnée";
 	public static final String DAL_UPDATE_ARTICLE_SQLEXCEPTION = "Erreur lors de la mise à jour de l'article dans la base de donnée";
 	public static final String DAL_SELECT_ARTICLE_SQLEXCEPTION = "Erreur lors de l'obtention d'articles dans la base de donnée";
-	public static final String DAL_DELETE_ARTICLE_SQLEXCEPTION = "Erreur lors de la supprésion de l'article dans la base de donnée";
+	public static final String DAL_DELETE_ARTICLE_SQLEXCEPTION = "Erreur lors de la suppression de l'article dans la base de donnée";
 	
 	public static final String DAL_INSERT_BID_SQLEXCEPTION = "Erreur lors de l'insertion de l'enchère dans la base de donnée";
 	public static final String DAL_SELECT_BID_SQLEXCEPTION = "Erreur lors de l'obtention d'enchères dans la base de donnée";
+	
+	public static final String BLL_LOGIN_USER_EXCEPTION = "Erreur lors de votre connexion ; vérifiez votre pseudo ou mot de passe";
+	public static final String BLL_PWD_USER_EXCEPTION = "Il y a une erreur sur la vérification du mot de passe : veuillez essayer à nouveau";		
 	
 	public BusinessException() {
 	}
