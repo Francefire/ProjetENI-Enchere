a5734b8477ebd14c7676a1852a65d02061f76393
Maxime <maxime.michaud2023@campus-eni.fr>
2024-03-04 15:06:58 +0100

    Ajout d'une classe utils avec des méthodes communes aux Managers

--- src/main/java/fr/eni/projetencheres/bll/BusinessException.java
+++ src/main/java/fr/eni/projetencheres/bll/BusinessException.java
@@ -2,30 +2,34 @@ package fr.eni.projetencheres.bll;
 
 public class BusinessException extends Exception {
 	private static final long serialVersionUID = 1L;
-	
+
 	public static final String DAL_INSERT_ARTICLE_SQLEXCEPTION = "Erreur lors de l'insertion de l'article dans la base de données";
 	public static final String DAL_UPDATE_ARTICLE_SQLEXCEPTION = "Erreur lors de la mise à jour de l'article dans la base de données";
 	public static final String DAL_SELECT_ARTICLE_SQLEXCEPTION = "Erreur lors de l'obtention d'articles dans la base de données";
 	public static final String DAL_DELETE_ARTICLE_SQLEXCEPTION = "Erreur lors de la suppréssion de l'article dans la base de données";
-	
+
 	public static final String DAL_INSERT_BID_SQLEXCEPTION = "Erreur lors de l'insertion de l'enchère dans la base de données";
 	public static final String DAL_SELECT_BID_SQLEXCEPTION = "Erreur lors de l'obtention d'enchères dans la base de données";
-  
-	public static final String DAL_INSERT_USER_SQLEXCEPTION = "Erreur lors de la création du compte" ;
-  
-	
+
+	public static final String DAL_INSERT_USER_SQLEXCEPTION = "Erreur lors de la création du compte";
+
 	public static final String BLL_LOGIN_USER_EXCEPTION = "Erreur lors de votre connexion ; vérifiez votre pseudo ou mot de passe";
 	public static final String BLL_PWD_USER_EXCEPTION = "Il y a une erreur sur la vérification du mot de passe : veuillez essayer à nouveau";
-  public static final String BLL_ARTICLE_NULL = "Aucun articles n'a pu être trouvé";
-	public static final String BLL_ARTICLES_NULL = "Aucun articles n'ont pu être trouvés";
-	
-	public static final String BLL_EMPTY_FIELDS = "Champ non renseigné";
 
-	public static final String BLL_BID_AMOUNT_NOT_ENOUGH = "Le montant de l'enchère doit être supérieur ou égale à un";
-	public static final String BLL_BID_ENDED = "L'enchère est terminée, vous ne pouvez plus enchérir sur celle-ci";
+	public static final String BLL_ADD_ARTICLE_START_DATE_AFTER_END_DATE_ERROR = "La date du début de l'enchère ne peut pas être après la fin de celle-ci";
+	public static final String BLL_ADD_ARTICLE_PRICE_NOT_ENOUGH_ERROR = "Le prix initial de l'article doit être supérieur ou égal à zéro";
+	public static final String BLL_GET_ARTICLE_NULL = "L'article n'a pu être trouvé";
+	public static final String BLL_GET_ALL_ARTICLES_NULL = "Aucuns articles n'ont pu être trouvés";
+
+	public static final String BLL_FIELD_EMPTY_ERROR = "Le %s doit être renseigné";
+	public static final String BLL_FIELD_BOUNDS_ERROR = "Le %s doit être compris entre %d et %d caractères";
+	public static final String BLL_EMPTY_FIELDS_ERROR = "Tous les champs doivent être renseignés";
+
+	public static final String BLL_ADD_BID_AMOUNT_NOT_ENOUGH_ERROR = "Le montant de l'enchère doit être supérieur ou égal à un";
+	public static final String BLL_ADD_BID_ENDED_ERROR = "L'enchère est terminée, vous ne pouvez plus enchérir sur celle-ci";
+
 	public static final String BLL_ERROR_SQLEXCEPTION_LOGIN = "Il y a un probleme avec votre statut -en ligne- !";
 
-	
 	public BusinessException() {
 	}
 
