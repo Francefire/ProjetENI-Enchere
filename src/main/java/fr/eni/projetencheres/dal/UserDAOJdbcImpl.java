dabeed0dfd0727d29af5a8fa0396429be7f414d9
Maxime <maxime.michaud2023@campus-eni.fr>
2024-03-06 16:17:57 +0100

    Correction bug lors du merge category dans main

--- src/main/java/fr/eni/projetencheres/dal/UserDAOJdbcImpl.java
+++ src/main/java/fr/eni/projetencheres/dal/UserDAOJdbcImpl.java
@@ -1,6 +1,7 @@
 package fr.eni.projetencheres.dal;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
