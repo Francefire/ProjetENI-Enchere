2223340469ec39e4d844d8b5798401381fe30e22
Maxime <maxime.michaud2023@campus-eni.fr>
2024-03-07 17:24:54 +0100

    Ajout des Retraits

--- src/main/java/fr/eni/projetencheres/dal/CategoriesDAO.java
+++ src/main/java/fr/eni/projetencheres/dal/CategoriesDAO.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import fr.eni.projetencheres.bll.BusinessException;
 import fr.eni.projetencheres.bo.Category;
 
 public class CategoriesDAO {
